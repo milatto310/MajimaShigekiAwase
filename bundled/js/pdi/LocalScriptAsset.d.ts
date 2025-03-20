fcd238268b4d4d99d4538a97a22afd4982d1a53b
milatto310 <68056270+milatto310@users.noreply.github.com>
2025-03-20 23:54:36 +0900

    Delete bundled directory

--- bundled/js/pdi/LocalScriptAsset.d.ts
+++
@@ -1,6 +0,0 @@
-declare class LocalScriptAsset extends g.ScriptAsset {
-    func: Function;
-    constructor(id: string, path: string);
-    _load(loader: AssetLoadHandler): void;
-    execute(execEnv: ScriptAssetExecuteEnvironment): any;
-}
